996ced1dff3447272b6ec32ee8631dacda47c819
Rasmus Wittrock <rasmus.wittrock@gmail.com>
2023-11-23 11:49:22 +0700

    fixed outlier detection

--- cleandata.py
+++ cleandata.py
@@ -1,20 +1,4 @@
 import pandas as pd
-import numpy as np
-
-
-def remove_outliers_iqr(data, column):
-    q1 = data[column].quantile(0.25)
-    q3 = data[column].quantile(0.75)
-    iqr = q3 - q1
-    lower_bound = q1 - 1.5 * iqr
-    upper_bound = q3 + 1.5 * iqr
-    data[column] = np.where(
-        (data[column] < lower_bound) | (data[column] > upper_bound),
-        np.nan,
-        data[column],
-    )
-    return data
-
 
 # Read the CSV file
 file_path = "./datasets/raw_data.csv"
@@ -22,25 +6,6 @@ data = pd.read_csv(file_path)
 
 # Round down all numbers in the "chlorides" column to 3 decimals
 data["chlorides"] = data["chlorides"].round(3)
-
-# Remove outliers using the IQR method for specified columns
-columns_to_process = [
-    "fixed acidity",
-    "volatile acidity",
-    "citric acid",
-    "residual sugar",
-    "chlorides",
-    "free sulfur dioxide",
-    "total sulfur dioxide",
-    "density",
-    "pH",
-    "sulphates",
-    "alcohol",
-    "quality",
-]
-
-for column in columns_to_process:
-    data = remove_outliers_iqr(data, column)
 
 # Replace missing values with the mean of each column
 data = data.fillna(data.mean())
